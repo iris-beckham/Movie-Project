0d685b1bb9b34e422fb609d2eed36f3946e1b965
Iris Beckham <irisdancer3@gmail.com>
2023-10-16 12:00:39 -0700

    able to add movies to cart.json by id

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const { readJSONFile, writeJSONFile } = require('./src/helpers');
 const {create, createMovies, index, info, destroy, editName} = require('./src/movieController')
+const {addToCart} = require('./src/cartController')
 
 const movies = readJSONFile('./data', 'movies.json');
+const cart = readJSONFile('./data', 'cart.json');
 
 const run = () => {
     const action = process.argv[2];
     const arg = process.argv[3];
 
-    let writeToFile = false;
-    let updatedMovies = [];
+    let writeToMoviesFile, writeToCartFile = false;
+    let updatedMovies, updatedCart = [];
 
     switch (action) {
         case "create":
             updatedMovies = arg ? createMovies(movies, arg) : create(movies);
-            writeToFile = true;
+            writeToMoviesFile = true;
             break;
         case "index":
             console.log(index(movies))
@@ -23,17 +25,24 @@ const run = () => {
             break;
         case "update":
             updatedMovies = editName(movies, arg,  process.argv[4])
-            writeToFile = true;
+            writeToMoviesFile = true;
             break;
         case "delete":
             updatedMovies = destroy(movies, arg);
-            writeToFile = true;
+            writeToMoviesFile = true;
+            break;
+        case "addToCart":
+            updatedCart = addToCart(movies, cart, arg);
+            writeToCartFile = true;
             break;
         default:
             console.log("There was an error.")
     }
-    if(writeToFile){
+    if(writeToMoviesFile){
         writeJSONFile('./data', 'movies.json', updatedMovies);
+    }
+    if(writeToCartFile){
+        writeJSONFile('./data', 'cart.json', updatedCart);
     }
 }
 
