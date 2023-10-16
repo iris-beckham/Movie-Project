cf0efde6962a90e2da06d7fda712999aa7423624
Iris Beckham <irisdancer3@gmail.com>
2023-10-16 10:22:11 -0700

    switch case set up for run function

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ const { readJSONFile, writeJSONFile } = require('./src/helpers');
 
 const movies = readJSONFile('./data', 'movies.json');
 
+const run = () => {
+    const action = process.argv[2];
+
+    let writeToFile = false;
+    let updatedMovies = [];
+
+    switch (action) {
+        case "create":
+            console.log(action, movies)
+            writeToFile = true;
+            break;
+        case "read":
+            console.log(action, movies)
+            break;
+        case "update":
+            console.log(action, movies)
+            writeToFile = true;
+            break;
+        case "destroy":
+            console.log(action, movies)
+            writeToFile = true;
+            break;
+    }
+}
+
+run();
