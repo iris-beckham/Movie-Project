0864c7c637a2134d380664c671359c7c2869a4c2
Iris Beckham <irisdancer3@gmail.com>
2023-10-16 12:04:43 -0700

    cart will not add movie if it is not in stock

--- src/cartController.js
+++ src/cartController.js
@@ -1,14 +1,15 @@
 
 const addToCart = (movies, cart, movieID) => {
     const index = movies.findIndex((movie) => movie.id === movieID);
-    if (index > -1) {
+    if (index > -1 && movies[index].inStock) {
         cart.push(movies[index]);
         console.log(`${movies[index].name} successfully added to cart`)
         return cart;
-    } else {
+    } else if (!movies[index].inStock) {
+        console.log(`${movies[index].name} is not currently in stock`)
+    } else
         console.log(`Movie with id ${movieID} not found. No action taken`);
-        return cart;
-    }
+    return cart;
 }
 
 module.exports = { addToCart }
