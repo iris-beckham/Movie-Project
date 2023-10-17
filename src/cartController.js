87a9ac73522a474f4d301a9ff076dba2697086da
Iris Beckham <irisdancer3@gmail.com>
2023-10-16 17:09:32 -0700

    updated addToCart function to count total number of each item

--- src/cartController.js
+++ src/cartController.js
@@ -3,25 +3,31 @@ const addToCart = (movies, cart, movieID) => {
     const index = movies.findIndex((movie) => movie.id === movieID);
     if (index > -1) {
         if (movies[index].inStock) {
-            cart.push(movies[index]);
-            console.log(`${movies[index].name} successfully added to cart. $${movies[index].priceInCents/100.00}`);
+            if (cart.find((movie) => movie.id === movieID)) {
+                cart[cart.findIndex((movie) => movie.id === movieID)].quantity++;
+                cart[cart.length - 1].priceInCents *= 2;
+            } else {
+                cart.push(movies[index]);
+                cart[cart.length - 1].quantity = 1;
+            }
+            console.log(`${movies[index].name} successfully added to cart. $${movies[index].priceInCents / 100.00}`);
             return cart;
-        } else{
+        } else {
             console.log(`${movies[index].name} is not currently in stock`)
         }
-    } else{
+    } else {
         console.log(`Movie with id ${movieID} not found. No action taken`);
     }
     return cart;
 }
 
 const totalCost = (cart) => {
-    const total =  cart.reduce((sum, movie) => sum + Number(movie.priceInCents), cart[0].priceInCents ) - cart[0].priceInCents;
-    return `$${total/100.00}`;
+    const total = cart.reduce((sum, movie) => sum + Number(movie.priceInCents), cart[0].priceInCents) - cart[0].priceInCents;
+    return `$${total / 100.00}`;
 }
 
 const showCart = (cart) => {
-    return cart.map((movie) => '$'+movie.priceInCents/100.00 + ' ' + movie.name).join('\n') + '\nTotal: ' + totalCost(cart);
+    return cart.map((movie) => '$' + movie.priceInCents / 100.00 + ' ' + movie.quantity + ' ' + movie.name).join('\n') + '\nTotal: ' + totalCost(cart);
 }
 
 module.exports = { addToCart, totalCost, showCart }
